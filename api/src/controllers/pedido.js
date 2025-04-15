80a2a57896d39a733e9289b8b28857fa539ffa9c
CrislaineL <crislaineleopoldocandido@gmail.com>
2025-04-15 17:09:12 -0300

    Finalizado

--- api/src/controllers/pedido.js
+++ api/src/controllers/pedido.js
@@ -29,7 +29,7 @@ const readOne = async (req, res) => {
             pedido_id: Number(req.params.id)
         },
         include: {
-            cliente: true,
+            motorista: true,
           
         }
     });
